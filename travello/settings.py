4fb3a8dbbccf7094d3fece1ba70ebc6d2ce9ca05
sayildar <sayildar17@gmail.com>
2024-07-18 00:23:13 +0500

    added allowed host and static conf

--- travello/settings.py
+++ travello/settings.py
@@ -11,7 +11,10 @@ SECRET_KEY = 'django-insecure-za7%24*32r%#r$!^25znrlooo5=5sz=dh(owsph%7f!g^z9&b$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'travelloapi.pythonanywhere.com',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -96,6 +99,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
