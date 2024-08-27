4d3cb506d7bfca7dbecc04aaf002489a5a336bfa
IldarSaygafarov2 <sayildar17@gmail.com>
2024-08-27 13:38:36 +0500

    added fixes

--- apps/tours_booked/apps.py
+++ apps/tours_booked/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ToursBookedConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tours_booked'
+    verbose_name = 'Забронированные туры'
--- apps/tours_favorites/apps.py
+++ apps/tours_favorites/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ToursFavoritesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.tours_favorites'
+    verbose_name = 'Избранные туры'
