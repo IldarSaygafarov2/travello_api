a78ed79ef4ba831838e4a8e8f282c89d2e145d30
IldarSaygafarov2 <sayildar17@gmail.com>
2025-01-12 16:03:23 +0500

    added fixes

--- apps/users_auth/serializers.py
+++ apps/users_auth/serializers.py
@@ -12,20 +12,20 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'password1', 'password2']
+        fields = ["username", "email", "phone_number", "password1", "password2"]
 
     def validate(self, data):
-        password1 = data.pop('password1')
-        password2 = data.pop('password2')
+        password1 = data.pop("password1")
+        password2 = data.pop("password2")
 
         if password1 != password2:
-            raise serializers.ValidationError({'detail': 'passwords do not match'})
+            raise serializers.ValidationError({"detail": "passwords do not match"})
 
-        data['password'] = password2
+        data["password"] = password2
         return data
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
+        password = validated_data.pop("password", None)
         user = User.objects.create_user(**validated_data)
         user.set_password(password)
         verification_code = generate_code()
@@ -46,31 +46,35 @@ class UserLoginSerializer(serializers.Serializer):
     token = serializers.CharField(read_only=True)
 
     def validate(self, data):
-        username = data.get('username', None)
-        password = data.get('password', None)
+        username = data.get("username", None)
+        password = data.get("password", None)
 
         if username is None:
-            raise serializers.ValidationError({'detail': 'Username is required'})
+            raise serializers.ValidationError({"detail": "Username is required"})
 
         if password is None:
-            raise serializers.ValidationError({'detail': 'Password is required'})
+            raise serializers.ValidationError({"detail": "Password is required"})
 
         user = authenticate(username=username, password=password)
 
         if user is None:
-            raise serializers.ValidationError({'detail': 'Username or Password is invalid'})
+            raise serializers.ValidationError(
+                {"detail": "Username or Password is invalid"}
+            )
 
         if not user.is_active:
-            raise serializers.ValidationError({'detail': 'User account is disabled'})
+            raise serializers.ValidationError({"detail": "User account is disabled"})
 
-        if not user.is_verified:
-            raise serializers.ValidationError({'detail': 'User account is not verified'})
+        # if not user.is_verified:
+        #     raise serializers.ValidationError(
+        #         {"detail": "User account is not verified"}
+        #     )
 
         return {
-            'user_id': user.id,
-            'username': user.username,
-            'user_type': user.user_type,
-            'token': user.token,
+            "user_id": user.id,
+            "username": user.username,
+            "user_type": user.user_type,
+            "token": user.token,
         }
 
 
