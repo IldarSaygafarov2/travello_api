dbc00cdee098ec6db1a1d0da6c37e92c9e64553f
IldarSaygafarov2 <sayildar17@gmail.com>
2025-01-13 20:07:56 +0500

    added fixes

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin, TranslationStackedInline
-from nested_inline.admin import NestedTabularInline, NestedModelAdmin, NestedStackedInline
+from nested_inline.admin import (
+    NestedTabularInline,
+    NestedModelAdmin,
+    NestedStackedInline,
+)
 
 from . import models
 
@@ -20,34 +24,50 @@ class EventPriceIncludedInline(NestedStackedInline, TranslationStackedInline):
     extra = 1
 
 
+class EventImportantOptionInline(NestedStackedInline, TranslationStackedInline):
+    model = models.EventImportantOption
+    extra = 1
+
+
 class EventPriceNotIncludedInline(NestedStackedInline, TranslationStackedInline):
     model = models.EventPriceNotIncluded
     extra = 1
 
 
 class EventTourProgramAdmin(NestedStackedInline, TranslationStackedInline):
-    fields = ['day', 'title', 'description']
+    fields = ["day", "title", "description"]
     model = models.EventTourProgram
     inlines = [EventTourProgramGalleryInline]
     extra = 1
 
 
 class EventAdmin(NestedModelAdmin, TranslationAdmin):
-    list_display = ['pk', 'title', 'price', 'corporate_client_price', 'country', 'event_start', 'event_end', 'event_type']
-    list_display_links = ['pk', 'title']
-    list_editable = ['price', 'event_type', 'event_start', 'event_end', ]
-    list_filter = ['event_type']
+    list_display = [
+        "pk",
+        "title",
+        "price",
+        "corporate_client_price",
+        "country",
+        "event_start",
+        "event_end",
+        "event_type",
+    ]
+    list_display_links = ["pk", "title"]
+    list_editable = [
+        "price",
+        "event_type",
+        "event_start",
+        "event_end",
+    ]
+    list_filter = ["event_type"]
     inlines = [
         EventGalleryInline,
         EventTourProgramAdmin,
         EventPriceIncludedInline,
         EventPriceNotIncludedInline,
+        EventImportantOptionInline,
     ]
-    prepopulated_fields = {'slug': ('title',)}
-
-
+    prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(models.Event, EventAdmin)
-
-
