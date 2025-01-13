d798e51965ebcce6d53cb7901982fa54e3f8c92d
IldarSaygafarov2 <sayildar17@gmail.com>
2025-01-13 20:10:41 +0500

    added fixes

--- apps/events/translation.py
+++ apps/events/translation.py
@@ -3,36 +3,37 @@ from . import models
 
 
 class EventTourProgramTranslationOptions(TranslationOptions):
-    fields = (
-        'title',
-        'description'
-    )
+    fields = ("title", "description")
 
 
 class EventPriceIncludedTranslationOptions(TranslationOptions):
-    fields = (
-        'title',
-    )
+    fields = ("title",)
 
 
 class EventPriceNotIncludedTranslationOptions(TranslationOptions):
-    fields = (
-        'title',
-    )
+    fields = ("title",)
+
+
+class EventImportantOptionTranslationOptions(TranslationOptions):
+    fields = ("title",)
 
 
 class EventTranslationOptions(TranslationOptions):
     fields = (
-        'title',
-        'short_description',
-        'full_description',
-        'country_from',
-        'country',
-        'gathering_place',
+        "title",
+        "short_description",
+        "full_description",
+        "country_from",
+        "country",
+        "gathering_place",
     )
 
 
 translator.register(models.Event, EventTranslationOptions)
 translator.register(models.EventPriceIncluded, EventPriceIncludedTranslationOptions)
-translator.register(models.EventPriceNotIncluded, EventPriceNotIncludedTranslationOptions)
+translator.register(models.EventImportantOption, EventImportantOptionTranslationOptions)
+translator.register(
+    models.EventPriceNotIncluded,
+    EventPriceNotIncludedTranslationOptions,
+)
 translator.register(models.EventTourProgram, EventTourProgramTranslationOptions)
