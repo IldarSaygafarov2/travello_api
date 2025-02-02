2df18cdc9188cb6cab0248450288acd05297341e
IldarSaygafarov2 <sayildar17@gmail.com>
2025-02-02 13:39:48 +0500

    added fixes

--- apps/hotels/views.py
+++ apps/hotels/views.py
@@ -6,40 +6,45 @@ from rest_framework.response import Response
 from . import models, serializers
 
 
-@extend_schema(tags=['Hotels'])
+@extend_schema(tags=["Hotels"])
 class HotelListView(generics.ListAPIView):
     queryset = models.Hotel.objects.all()
+
     serializer_class = serializers.HotelListSerializer
-    filter_backends = (filters.OrderingFilter, DjangoFilterBackend,)
+    filter_backends = (
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    )
+    filterset_fields = ("hotel_type",)
 
 
-@extend_schema(tags=['Hotels'])
+@extend_schema(tags=["Hotels"])
 class HotelDetailView(generics.RetrieveAPIView):
     queryset = models.Hotel.objects.all()
     serializer_class = serializers.HotelDetailSerializer
-    lookup_url_kwarg = 'hotel_id'
+    lookup_url_kwarg = "hotel_id"
 
 
-@extend_schema(tags=['Hotels'])
+@extend_schema(tags=["Hotels"])
 class HotelRoomsListView(generics.ListAPIView):
     queryset = models.HotelRoom.objects.all()
     serializer_class = serializers.HotelRoomSerializer
-    lookup_url_kwarg = 'hotel_id'
+    lookup_url_kwarg = "hotel_id"
 
     def get_queryset(self):
-        hotel_id = self.kwargs.get('hotel_id')
+        hotel_id = self.kwargs.get("hotel_id")
         return models.HotelRoom.objects.filter(hotel_id=hotel_id)
 
 
-@extend_schema(tags=['Hotels'])
+@extend_schema(tags=["Hotels"])
 class HotelRoomDetailView(generics.RetrieveAPIView):
     queryset = models.HotelRoom.objects.all()
     serializer_class = serializers.HotelRoomSerializer
-    lookup_url_kwarg = 'room_id'
+    lookup_url_kwarg = "room_id"
 
     def retrieve(self, request, *args, **kwargs):
-        hotel_id = kwargs.get('hotel_id')
-        room_id = kwargs.get('room_id')
+        hotel_id = kwargs.get("hotel_id")
+        room_id = kwargs.get("room_id")
 
         hotel_rooms = models.HotelRoom.objects.filter(hotel_id=hotel_id)
         if not hotel_rooms:
